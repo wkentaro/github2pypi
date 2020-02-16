4cb0727d04640fbb38fe79f6c7a63555364f4a40
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-02-16 19:36:26 +0000

    Use black

--- replace_url.py
+++ replace_url.py
@@ -1,22 +1,20 @@
 import re
 
 
-def replace_url(slug, content, branch='master'):
-
+def replace_url(slug, content, branch="master"):
     def repl(match):
         if not match:
             return
 
         url = match.group(1)
-        if url.startswith('http'):
+        if url.startswith("http"):
             return match.group(0)
 
-        url_new = (
-            'https://github.com/{slug}/blob/{branch}/{url}'
-            .format(slug=slug, branch=branch, url=url)
+        url_new = "https://github.com/{slug}/blob/{branch}/{url}".format(
+            slug=slug, branch=branch, url=url
         )
-        if re.match(r'.*[\.jpg|\.png|\.gif]$', url_new):
-            url_new += '?raw=true'
+        if re.match(r".*[\.jpg|\.png|\.gif]$", url_new):
+            url_new += "?raw=true"
 
         start0, end0 = match.regs[0]
         start, end = match.regs[1]
@@ -30,12 +28,12 @@ def replace_url(slug, content, branch='master'):
     lines = []
     for line in content.splitlines():
         patterns = [
-            r'!\[.*?\]\((.*?)\)',
+            r"!\[.*?\]\((.*?)\)",
             r'<img.*?src="(.*?)".*>',
-            r'\[.*?\]\((.*?)\)',
+            r"\[.*?\]\((.*?)\)",
             r'<a.*?href="(.*?)".*?>',
         ]
         for pattern in patterns:
             line = re.sub(pattern, repl, line)
         lines.append(line)
-    return '\n'.join(lines)
+    return "\n".join(lines)
